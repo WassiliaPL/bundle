c87565560539bd1043442a98068471040bc4517d
dawidd6 <dawidd0811@gmail.com>
2016-01-31 13:44:27 +0100

    added caesar cipher + update

--- bundle.cpp
+++ bundle.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cstdlib>
-//#include <locale>
+
 using namespace std;
 
 #include "src/dec2all.hpp"
@@ -9,19 +9,29 @@ using namespace std;
 #include "src/help.hpp"
 #include "src/schedule.hpp"
 #include "src/exponentiation.hpp"
+#include "src/caesar_cipher.hpp"
 
 int main (int argc, char *argv[])
 {
-	//cout << "User-preferred locale setting is " << std::locale("").name().c_str() << '\n';
 	if (argc == 1) help();
 	else
 	{
 		string arg = argv[1];
 		if ((arg == "-h") or (arg == "--help")) help();
-		else if ((arg == "-p") or (arg == "--prime")) prime_numbers (argv[2]);
+		else if ((arg == "-p") or (arg == "--primenumbers")) prime_numbers (argv[2]);
 		else if ((arg == "-f") or (arg == "--factorial")) factorial (argv[2]);
-		else if ((arg == "-d") or (arg == "--decimal2all")) dec2all (argv[2], argv[3]);
+		else if ((arg == "-d2a") or (arg == "--decimal2all")) dec2all (argv[2], argv[3]);
 		else if ((arg == "-s") or (arg == "--schedule")) schedule (argv[2]);
 		else if ((arg == "-e") or (arg == "--exponentiation")) exponentiation (argv[2], argv[3]);
+		else if ((arg == "-cd") or (arg == "--caesardecode"))
+		{
+			for (int i = 2; i<argc; i++) caesar_decode (argv[i]);
+			cout << endl;
+		}
+		else if ((arg == "-ce") or (arg == "--caesarencode"))
+		{
+			for (int i = 2; i<argc; i++) caesar_encode (argv[i]);
+			cout << endl;
+		}
 	}
 }
