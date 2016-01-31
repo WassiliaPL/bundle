b2871f89a02f9f5a791e0af8c01378766b4854c9
dawidd6 <dawidd0811@gmail.com>
2016-01-31 22:31:15 +0100

    changed USAGE + added full morse

--- bundle.cpp
+++ bundle.cpp
@@ -21,7 +21,7 @@ int main (int argc, char *argv[])
 		if ((arg == "-h") or (arg == "--help")) help();
 		else if ((arg == "-p") or (arg == "--primenumbers")) prime_numbers (argv[2]);
 		else if ((arg == "-f") or (arg == "--factorial")) factorial (argv[2]);
-		else if ((arg == "-d2a") or (arg == "--decimal2all")) dec2all (argv[2], argv[3]);
+		else if ((arg == "-d") or (arg == "--decimal2all")) dec2all (argv[2], argv[3]);
 		else if ((arg == "-s") or (arg == "--schedule")) schedule (argv[2]);
 		else if ((arg == "-e") or (arg == "--exponentiation")) exponentiation (argv[2], argv[3]);
 		else if ((arg == "-cd") or (arg == "--caesardecode"))
