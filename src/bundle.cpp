3ba679a8cd16bf17ee99f2b6d97866626474415e
dawidd6 <dawidd0811@gmail.com>
2016-10-10 18:16:18 +0200

    limited allowed arguments

--- src/bundle.cpp
+++ src/bundle.cpp
@@ -7,12 +7,12 @@ int main (int argc, char *argv[])
 	{
 		string arg = argv[1];
 		if ((arg == "-h") or (arg == "--help")) help();
-		else if ((arg == "-p") or (arg == "--primenumbers")) prime_numbers (argv[2]);
-		else if ((arg == "-f") or (arg == "--factorial")) factorial (argv[2]);
-		else if ((arg == "-d") or (arg == "--decimal2all")) dec2all (argv[2], argv[3]);
-		else if ((arg == "-a") or (arg == "--all2decimal")) all2dec (argv[2], argv[3]);
-		else if ((arg == "-s") or (arg == "--schedule")) schedule (argv[2]);
-		else if ((arg == "-e") or (arg == "--exponentiation")) exponentiation (argv[2], argv[3]);
+		else if (((arg == "-p") or (arg == "--primenumbers")) and (argc > 2) and (argc < 4)) prime_numbers (argv[2]);
+		else if (((arg == "-f") or (arg == "--factorial")) and (argc > 2) and (argc < 4)) factorial (argv[2]);
+		else if (((arg == "-d") or (arg == "--decimal2all")) and (argc > 3) and (argc < 5)) dec2all (argv[2], argv[3]);
+		else if (((arg == "-a") or (arg == "--all2decimal")) and (argc > 3) and (argc < 5)) all2dec (argv[2], argv[3]);
+		else if (((arg == "-s") or (arg == "--schedule")) and (argc > 2) and (argc < 4)) schedule (argv[2]);
+		else if (((arg == "-e") or (arg == "--exponentiation")) and (argc > 3) and (argc < 5)) exponentiation (argv[2], argv[3]);
 		else if ((arg == "-cd") or (arg == "--caesardecode"))
 		{
 			for (int i = 2; i<argc; i++) caesar_decode (argv[i]);
@@ -47,17 +47,18 @@ int main (int argc, char *argv[])
 			min_max (t, argc-2);
 			cout << endl;
 		}
-		else if ((arg == "-n") or (arg == "--fibonacci")) fibonacci (argv[2]);
-		else if ((arg == "-eu") or (arg == "--euklides")) euklides (argv[2], argv[3]);
-		else if ((arg == "-r") or (arg == "--readfile")) fileoutput (argv[2]);
-		else if ((arg == "-w") or (arg == "--writetofile")) fileinput (argv[2], argv[3]);
-		else if ((arg == "-ff") or (arg == "--findinfile")) filefind (argv[2], argv[3]);
-		else if ((arg == "-ar") or (arg == "--armstrong")) armstrong (argv[2]);
-		else if ((arg == "-pd") or (arg == "--palindrome")) palindrome (argv[2]);
+		else if (((arg == "-n") or (arg == "--fibonacci")) and (argc > 2) and (argc < 4)) fibonacci (argv[2]);
+		else if (((arg == "-eu") or (arg == "--euklides")) and (argc > 3) and (argc < 5)) euklides (argv[2], argv[3]);
+		else if (((arg == "-r") or (arg == "--readfile")) and (argc > 2) and (argc < 4)) fileoutput (argv[2]);
+		else if (((arg == "-w") or (arg == "--writetofile")) and (argc > 3) and (argc < 5)) fileinput (argv[2], argv[3]);
+		else if (((arg == "-ff") or (arg == "--findinfile")) and (argc > 3) and (argc < 5)) filefind (argv[2], argv[3]);
+		else if (((arg == "-ar") or (arg == "--armstrong")) and (argc > 2) and (argc < 4)) armstrong (argv[2]);
+		else if (((arg == "-pd") or (arg == "--palindrome")) and (argc > 2) and (argc < 4)) palindrome (argv[2]);
 		else
 		{
 			cout << "bundle: wrong argument\n" << "Write „bundle --help” for informations\n";
 			return 1;
 		}
 	}
+	return 0;
 }
