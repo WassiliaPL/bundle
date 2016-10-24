a12753deef7cceebd0b055cc4369824433ec2928
WassiliaPL <wojcikmikolaj1@gmail.com>
2016-10-24 20:29:55 +0200

    naprawiono błąd zrzutu pamięci

--- src/dec2all.cpp
+++ src/dec2all.cpp
@@ -4,25 +4,30 @@ void dec2all (char *x, char *y)
 {
 	int d = atoi(x), k = atoi(y), output[100];
 	int temp = d, j, i = 0;
-	do
+	if(k>=2)
 	{
-		output[i] = temp%k;
-		temp = temp/k;
-		i++;
+		do
+		{
+			output[i] = temp%k;
+			temp = temp/k;
+			i++;
+		}
+		while (temp > 0);
+		j = i;
+		cout << d << "(10)" << " = ";
+		for (i=j-1; i>=0; i--)
+		{
+			if (output[i] == 10) cout << "A";
+			else if (output[i] == 11) cout << "B";
+			else if (output[i] == 12) cout << "C";
+			else if (output[i] == 13) cout << "D";
+			else if (output[i] == 14) cout << "E";
+			else if (output[i] == 15) cout << "F";
+			else
+			cout << output[i];
+		}
+		cout << "(" << k << ")" << endl;
 	}
-	while (temp > 0);
-	j = i;
-	cout << d << "(10)" << " = ";
-	for (i=j-1; i>=0; i--)
-	{
-		if (output[i] == 10) cout << "A";
-		else if (output[i] == 11) cout << "B";
-		else if (output[i] == 12) cout << "C";
-		else if (output[i] == 13) cout << "D";
-		else if (output[i] == 14) cout << "E";
-		else if (output[i] == 15) cout << "F";
-		else
-		cout << output[i];
-	}
-	cout << "(" << k << ")" << endl;
+	else cout << "This numeral system does not exist!" << endl; 
 }
+
