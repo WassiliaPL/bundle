bc3db1b9ca7769b4ac570f831bdaf8888fc8d6a0
WassiliaPL <wojcikmikolaj1@gmail.com>
2016-10-24 21:13:53 +0200

    extending dec2all function to the end of the alphabet - it can show up to 35 numeral system

--- src/dec2all.cpp
+++ src/dec2all.cpp
@@ -4,7 +4,8 @@ void dec2all (char *x, char *y)
 {
 	int d = atoi(x), k = atoi(y), output[100];
 	int temp = d, j, i = 0;
-	if(k>1)
+	if(k>35) cout << "Too large numeral system for this program!" << endl;
+	if(k>1 and k<36)
 	{
 		do
 		{
@@ -23,11 +24,31 @@ void dec2all (char *x, char *y)
 			else if (output[i] == 13) cout << "D";
 			else if (output[i] == 14) cout << "E";
 			else if (output[i] == 15) cout << "F";
+			else if (output[i] == 16) cout << "G";
+			else if (output[i] == 17) cout << "H";
+			else if (output[i] == 18) cout << "I";
+			else if (output[i] == 19) cout << "J";
+			else if (output[i] == 20) cout << "K";
+			else if (output[i] == 21) cout << "L";
+			else if (output[i] == 22) cout << "M";
+			else if (output[i] == 23) cout << "N";
+			else if (output[i] == 24) cout << "O";
+			else if (output[i] == 25) cout << "P";
+			else if (output[i] == 26) cout << "Q";
+			else if (output[i] == 27) cout << "R";
+			else if (output[i] == 28) cout << "S";
+			else if (output[i] == 29) cout << "T";
+			else if (output[i] == 30) cout << "U";
+			else if (output[i] == 31) cout << "V";
+			else if (output[i] == 32) cout << "W";
+			else if (output[i] == 33) cout << "X";
+			else if (output[i] == 34) cout << "Y";
+			else if (output[i] == 35) cout << "Z";
 			else
 			cout << output[i];
 		}
 		cout << "(" << k << ")" << endl;
 	}
-	else cout << "This numeral system does not exist!" << endl; 
+	if(k<2) cout << "This numeral system does not exist!" << endl; 
 }
 
